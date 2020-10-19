2a12ab6a2d6f52ae292b342e42e2f8dbc931b190
0ojixueseno0 <jz0ojiang@qq.com>
2020-10-19 20:30:09 +0800

    支持翻译Yaml文件

--- TJson.py
+++ TJson.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+# !/usr/bin/env python
 #_*_coding:utf-8_*_
 import requests   #http请求包
 import json       #json解析包
@@ -44,7 +44,7 @@ except PermissionError:
 
 
 #载入配置文件
-f = open("config.yml",'r',encoding='utf-8')
+f = open("config.t.yml",'r',encoding='utf-8')
 
 config = yaml.load(f.read(), Loader=yaml.FullLoader)
 
@@ -58,7 +58,7 @@ whitelist = config['Config']["Whitelist"]
 blacklist = config['Config']["Blacklist"]
 listmode = str(config['Config']['listmode'])
 
-jsonpath = ""
+filepath = ""
 
 def error_code_parse(code):
   if code == "52001":
@@ -87,6 +87,7 @@ def error_code_parse(code):
     return "认证未通过或未生效 请前往我的认证查看认证进度"
 
 def translate(untrans):
+  #判断内容是否为空
   if untrans == "":
     return untrans
   else:
@@ -103,7 +104,7 @@ def translate(untrans):
     final_sign = hashlib.md5(final_sign.encode(encoding='UTF-8')).hexdigest()
     my_url = api_url + '?appid=' + app_id + '&q=' + untrans + '&from=' + transfrom + '&to=' + transto + '&salt=' + salt + '&sign=' + final_sign
     result = requests.get(my_url,params={},headers={},)
-    # print(result.text)
+    #判断是否翻译成功
     if "error_code" not in json.loads(result.text).keys():
       output = json.loads(result.text)["trans_result"][0]["dst"]
       _repcode = repcode
@@ -118,70 +119,79 @@ def translate(untrans):
     time.sleep(1)
     return output.encode('unicode_escape').decode('unicode_escape')
 
-def saveFile (input):
-  saveFile = open("output.json", "w", encoding='utf-8')
-  saveFile.write(json.dumps(input,indent=4,ensure_ascii=False))
-  saveFile.close()
+def saveFile (input,mode,suffix):
+  if mode == "json":
+    saveFile = open("output" + suffix, "w", encoding='utf-8')
+    saveFile.write(json.dumps(input,indent=4,ensure_ascii=False))
+    saveFile.close()
+  elif mode == "yaml":
+    saveFile = open("output" + suffix, "w", encoding='utf-8')
+    output = yaml.dump(input)
+    output = output.encode().decode('unicode_escape')
+    print(type(output))
+    print(output)
+    saveFile.write(output)
+    saveFile.close()
 
-# def list_parse(input):
-#   if isinstance(input, list):
-#     _elementnum = 0
-#     for element in list:
-#       if listinput[_elementnum]
-#       _elementnum = _elementnum +1
-
-def json_txt(dic_json):
-  # print(type(dic_json))
+def dict_parse(dic_json):
   if isinstance(dic_json,dict):
-    # print(type(dic_json))
     for key in dic_json:
-      # print(key)
       if (key in whitelist and listmode == "whitelist") or (key not in blacklist and listmode == "blacklist"):
-      # if isinstance(dic_json[key],dict):
-        # print("*"*30)
         if isinstance(dic_json[key],list):
           _elementnum = 0
           for element in dic_json[key]:
-            dic_json[key][_elementnum] = translate(str(element))
+            if isinstance(element,dict) or isinstance(element,list):
+              dict_parse(element)
+            else:
+              dic_json[key][_elementnum] = translate(str(element))
             _elementnum = _elementnum + 1
+        elif isinstance(dic_json[key],dict):
+          dict_parse(dic_json[key])
         else:
           dic_json[key] = translate(str(dic_json[key]))
       elif isinstance(dic_json[key],dict) or isinstance(dic_json[key],list):
-        json_txt(dic_json[key])
-      # elif isinstance(dic_json[key],list):
-      #   json_txt(dic_json[key])
+        dict_parse(dic_json[key])
   elif isinstance(dic_json,list):
-    # print("\n************\n")
     _elementnum = 0
     for element in dic_json:
-      # print(str(type(dic_json[_elementnum])) + "\n")
-      # for i in dic_json[key][_elementnum]:
-      #   print(i)
-      json_txt(dic_json[_elementnum])
+      dict_parse(dic_json[_elementnum])
       _elementnum = _elementnum + 1
 
 
 
 if len(sys.argv) == 2:
-  jsonpath = sys.argv[1]
-
+  filepath = sys.argv[1]
+  try:
+    f = open(filepath,'r',encoding='utf-8')
+    f.close()
+  except IOError:
+    print("[!]提示：打开文件出错 请检查文件是否存在或权限问题")
+    sys.exit()
+  f = open(filepath,'r',encoding='utf-8')
+  if filepath.endswith(".json"):
+    needtrans = json.loads(f.read())
+    dict_parse(needtrans)
+    saveFile(needtrans,"json",".json")
+    print("|-> 机翻完毕 请检查output.json的内容\n| =: Translate completed. Please Check 'output.json'")
+  elif filepath.endswith(".yaml") or filepath.endswith(".yml"):
+    if filepath.endswith(".yaml"):
+      suffix = ".yaml"
+    elif filepath.endswith(".yml"):
+      suffix = ".yml"
+    needtrans = yaml.load(f.read(), Loader=yaml.FullLoader)
+    dict_parse(needtrans)
+    saveFile(needtrans,"yaml",suffix)
+    print("|-> 机翻完毕 请检查output" + suffix + "的内容\n| =: Translate completed. Please Check 'output'"+suffix)
+  else:
+    print("[!]提示：文件 "+ str(filepath) + "不支持翻译")
+    sys.exit()
 elif len(sys.argv) == 1:
-  print("[*]错误 ==> 你需要提供需要翻译的文件地址\n\t||usage: *.py/*.exe 'test.json'")
+  print("[*]错误 ==> 你需要提供需要翻译的文件地址\n\t||usage: *.py/*.exe 'test.json' or 'test.yaml'")
   sys.exit()
 else:
-  print("[*]错误 ==> 请检查文件地址是否正确\n\t||usage: *.py/*.exe 'test.json'\n\t||文件地址带空格的请使用英文引号括起来")
+  print("[*]错误 ==> 请检查文件地址是否正确\n\t||usage: *.py/*.exe 'test.json' or 'test.yaml'\n\t||文件地址带空格的请使用英文引号括起来")
   sys.exit()
 
-f = open(jsonpath,'r',encoding='utf-8')
 
-# print(f.read())
-jsons = json.loads(f.read())
-# print(jsons)
 
-json_txt(jsons)
 
-# print(jsons)
-
-saveFile(jsons)
-
-print("|-> 机翻完毕 请检查output.json的内容\n| =: Translate completed. Please Check 'output.json'")
